676b2ee6b6857ba47575810ba14092ebdf7d1e93
David de la Puente <david.puente@marbleinteractive.com>
2023-02-11 09:55:53 +0100

    Create a mixing layout with Fix and Adaptive

---
+++ EssentialGridLayout/MixingGridsLayout.swift
@@ -0,0 +1,34 @@
+//
+//  MixingGridsLayout.swift
+//  EssentialGridLayout
+//
+//  Created by Marble Interactive SL on 11/2/23.
+//
+
+import SwiftUI
+
+struct MixingGridsLayout: View {
+    private var gridItemLayout = [GridItem(.fixed(150)), GridItem(.adaptive(minimum: 50 ))]
+    private var colors: [Color] = [.yellow, .purple, .green]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: gridItemLayout, spacing: 20) {
+                ForEach((0...999), id: \.self) {
+                    Image(systemName: symbols[$0 % symbols.count])
+                        .font(.system(size: 30))
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 50)
+                        .background(colors[$0 % colors.count])
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct MixingGridsLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        MixingGridsLayout()
+    }
+}
