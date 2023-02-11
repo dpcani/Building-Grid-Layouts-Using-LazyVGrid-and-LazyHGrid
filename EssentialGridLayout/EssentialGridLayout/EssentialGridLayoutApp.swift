8c24c2e1c35d7cc4a9bb4c477a6b70b0124a8d80
David de la Puente <david.puente@marbleinteractive.com>
2023-02-11 09:34:52 +0100

    Start project

---
+++ EssentialGridLayout/EssentialGridLayout/EssentialGridLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialGridLayoutApp.swift
+//  EssentialGridLayout
+//
+//  Created by Marble Interactive SL on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct EssentialGridLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
